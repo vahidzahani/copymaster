1f6d44e1fedcc5ac186b8977ead77fdcd5322cda
vahid PC OFFICE <vahid.zahani@gmail.com>
2024-01-03 18:36:11 +0330

    first commit

---
+++ copymaster/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace copymaster
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.ForeColor = System.Drawing.Color.Red;
+            this.textBox1.Location = new System.Drawing.Point(12, 25);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(680, 21);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "c:\\test\\";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox1.DoubleClick += new System.EventHandler(this.textBox1_DoubleClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 8);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Path:";
+            // 
+            // listBox1
+            // 
+            this.listBox1.AllowDrop = true;
+            this.listBox1.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 51);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
+            this.listBox1.Size = new System.Drawing.Size(784, 160);
+            this.listBox1.TabIndex = 5;
+            this.listBox1.DragDrop += new System.Windows.Forms.DragEventHandler(this.listBox1_DragDrop);
+            this.listBox1.DragEnter += new System.Windows.Forms.DragEventHandler(this.listBox1_DragEnter);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(15, 217);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(98, 29);
+            this.button3.TabIndex = 0;
+            this.button3.Text = "Clear";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button4.Location = new System.Drawing.Point(119, 217);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(98, 29);
+            this.button4.TabIndex = 0;
+            this.button4.Text = "Copy files";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button1.Location = new System.Drawing.Point(698, 8);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(98, 39);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Select path ...";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button2.Location = new System.Drawing.Point(698, 217);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(98, 29);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "About ...";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_2);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(808, 253);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Copymaster 1.0";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ copymaster/Form1.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+namespace copymaster
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            //this.listBox1.DragDrop += new System.Windows.Forms.DragEventHandler(this.listBox1_DragDrop);
+           // this.listBox1.DragEnter += new System.Windows.Forms.DragEventHandler(this.listBox1_DragEnter);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            
+        }
+
+        private void listBox1_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] s = (string[])e.Data.GetData(DataFormats.FileDrop, false);
+            int i;
+            for (i = 0; i < s.Length; i++)
+                listBox1.Items.Add(s[i]);
+        }
+
+        private void listBox1_DragEnter(object sender, DragEventArgs e)
+        {
+            //MessageBox.Show("ENTER");
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+                e.Effect = DragDropEffects.All;
+            else
+                e.Effect = DragDropEffects.None;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (listBox1.Items.Count==0)
+            {
+                MessageBox.Show("drag file to list box");                return;
+            }
+            string path_des = textBox1.Text;
+            if (path_des.Substring(path_des.Length - 1) != @"\") path_des += @"\";
+            if (!Directory.Exists(path_des))
+            {
+                MessageBox.Show("Path not found !");                return;
+            }
+
+
+           
+
+            string[] str_list = new string[listBox1.Items.Count];
+            for (int i = 0; i < listBox1.Items.Count; i++) 
+                str_list[i]=listBox1.Items[i].ToString();
+
+            string path_common = GetLongestCommonPrefix(str_list);
+
+            for (int i = 0; i < listBox1.Items.Count; i++)
+            {
+                string full_path = Path.GetDirectoryName(listBox1.Items[i].ToString());
+                string fullpath_file = listBox1.Items[i].ToString();
+                string file_name = Path.GetFileName(fullpath_file);
+                //start create direcory
+                fn_create_path(full_path, path_common, path_des);
+                //start copy files
+                string mid_path = "";
+                if (full_path != path_common)
+                    mid_path = full_path.Substring(path_common.Length + 1) + @"\";
+                string new_path = path_des + mid_path  + file_name;
+                File.Copy(fullpath_file, new_path ,true);
+
+            }
+            MessageBox.Show("Successfull");
+            System.Diagnostics.Process.Start(path_des);
+            
+            
+
+
+        }
+        
+        public static string GetLongestCommonPrefix(string[] s)
+        {
+            int k = s[0].Length;
+            for (int i = 1; i < s.Length; i++)
+            {
+                k = Math.Min(k, s[i].Length);
+                for (int j = 0; j < k; j++)
+                    if (s[i][j] != s[0][j])
+                    {
+                        k = j;
+                        break;
+                    }
+            }
+            int int_last_backslash = s[0].Substring(0, k).LastIndexOf(@"\");
+            return s[0].Substring(0, k).Substring(0,int_last_backslash);
+        }
+        
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            
+            
+
+        }
+        private void fn_create_path(string str_fullpath,string str_commonpath,string str_des)
+        {
+            if (str_fullpath.Length==str_commonpath.Length)
+            {
+                return;
+            }
+            string x = str_fullpath.Substring(str_commonpath.Length+1);
+            //MessageBox.Show(x);
+
+            string[] words = x.Split('\\');
+            string tmp = "";
+            for (int i = 0; i < words.Length; i++)
+            {
+                tmp+=words[i]+@"\";
+                Directory.CreateDirectory(str_des+tmp);
+                //MessageBox.Show(str_des+tmp);
+            }
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            folderBrowserDialog1.ShowDialog();
+            textBox1.Text = folderBrowserDialog1.SelectedPath;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_DoubleClick(object sender, EventArgs e)
+        {
+            if (Directory.Exists(textBox1.Text))
+            {
+                System.Diagnostics.Process.Start(textBox1.Text);
+            }
+            else
+            {
+                MessageBox.Show("not found");
+            }
+        }
+
+        private void button2_Click_2(object sender, EventArgs e)
+        {
+            MessageBox.Show("vahid.zahani@gmail.com");
+        }
+
+      
+    }
+}
